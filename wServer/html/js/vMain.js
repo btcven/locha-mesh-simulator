a74e858b9f85a6e5a9723c6491121df23ea3dcc3
luisan00 <luisan00@hotmail.com>
2019-03-02 09:09:49 +0100

    updated static files for webserver

--- wServer/html/js/vMain.js
+++ wServer/html/js/vMain.js
@@ -1,21 +1,19 @@
 var socket = io();
 
-const home = {
-    template: '#tpl_home'
-};
+var mymap = L.map('map-container').setView([51.505, -0.09], 13);
 
-const router = new VueRouter({
-    routes: [{
-        path: '/home',
-        component: home
-    }]
-});
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibHVpc2FuMDAiLCJhIjoiY2pzcWZ0aWFnMDFtNzQzcXEzMTlpc3ducSJ9.-iOStSAXx1QAnsiv8shgRg'
+}).addTo(mymap);
 
 const main = new Vue({
     el: "#main",
-    data: {
-        activeIndex: '1'
-    },
+    data: {},
     methods: {},
-    router
+    mounted: function(){
+        console.log('app mounted');
+    }
 });
